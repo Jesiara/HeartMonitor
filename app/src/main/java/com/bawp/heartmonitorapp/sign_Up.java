077ba595c94a4f360945eadcf856d0fbb41dd81f
Jesiara <jesiarakhatun2000@gmail.com>
2023-06-19 16:56:30 +0600

    Splash screen,Login page and Sign Up page designed

---
+++ app/src/main/java/com/bawp/heartmonitorapp/sign_Up.java
@@ -0,0 +1,14 @@
+package com.bawp.heartmonitorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sign_Up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
